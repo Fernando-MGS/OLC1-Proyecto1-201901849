c95826e5b045f7814cb128bbb112ac059ee5ea3e
Fernando-MGS <70865380+Fernando-MGS@users.noreply.github.com>
2021-09-08 14:18:27 -0600

    grafica2

--- OLC1/src/Graficas/Pie.java
+++ OLC1/src/Graficas/Pie.java
@@ -12,6 +12,8 @@ import org.jfree.chart.ChartRenderingInfo;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.entity.StandardEntityCollection;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 
 /**
@@ -24,13 +26,19 @@ public class Pie {
     }
     
     public void graf_Pie(){
-        DefaultPieDataset Gpie = new DefaultPieDataset();
-        Gpie.setValue("a", new Double(30));
+        //DefaultPieDataset Gpie = new DefaultPieDataset();
+        //JFreeChart chart= ChartFactory.createPieChart("Titulo", Gpie, true, true, true);
+        DefaultCategoryDataset Gpie = new DefaultCategoryDataset();
+        Gpie.setValue(1, "a", "1");
+        Gpie.setValue(2, "a", "2");
+        Gpie.setValue(3, "a", "3");
+        Gpie.setValue(4, "a", "4");
+        /*Gpie.setValue("a", new Double(30));
         //DefaultPieDataset.setValue("a", 0);
         Gpie.setValue("b", new Double (1));
         Gpie.setValue("c", new Double (2));
-        Gpie.setValue("d", new Double (4));
-        JFreeChart chart= ChartFactory.createPieChart("Titulo", Gpie, true, true, true);
+        Gpie.setValue("d", new Double (4));*/
+        JFreeChart chart= ChartFactory.createBarChart("Hola","Ejex","Ejey",Gpie,PlotOrientation.VERTICAL,true,true,false);
         try{
             final ChartRenderingInfo info = new ChartRenderingInfo(new StandardEntityCollection());
             final File file = new File("PIE"+AppForm.project_actual+".jpg");
